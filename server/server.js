799bb7edf7b4aba46e991136a499a763f34bd1e4
Astronaut004 <Astronaut004@users.noreply.github.com>
2025-08-03 00:47:20 +0530

    define routes

--- server/server.js
+++ server/server.js
@@ -2,11 +2,21 @@ import express from "express"
 import dotenv from "dotenv";
 import pool from "./models/db.js";
 import cors from "cors";
+import authRoute from './routes/authRoute.js';
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
+
+app.use(express.json());
+
+app.use('/api/auth',authRoute);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
 
 app.get('/', async (req,res) => {
