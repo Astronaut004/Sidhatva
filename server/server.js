6082a9c5fdfc4f2db8a921c3a8a255188f057b0f
Astronaut004 <Astronaut004@users.noreply.github.com>
2025-08-06 23:22:09 +0530

    set product routes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import pool from "./models/db.js";
 import cors from "cors";
 import authRoute from './routes/authRoute.js';
-
+import productRoute from './routes/productRoute.js';
 dotenv.config();
 
 const app = express();
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
+app.use('/api/products',productRoute);
 
 
 
