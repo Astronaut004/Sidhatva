48dd76698d77e2d7b08acba494f30c31b87d98e7
Garbhit Tyagi <garbhittyagi@gmail.com>
2025-08-16 19:59:33 +0530

    routes ,order,cart,orderitem

--- Routes/orderItemRoute.js
+++ Routes/orderItemRoute.js
@@ -1,22 +1,13 @@
-// routes/orderItemRoutes.js
 import express from 'express';
 import * as orderItemController from '../controllers/orderItemController.js';
 
 const router = express.Router();
 
-// Add an item to an order
+
 router.post('/', orderItemController.addOrderItem);
-
-// Get all items for a specific order
 router.get('/order/:orderId', orderItemController.getOrderItems);
-
-// Get a single order item by ID
 router.get('/:id', orderItemController.getOrderItemById);
-
-// Update an order item quantity
 router.put('/:id', orderItemController.updateOrderItem);
-
-// Remove an item from an order
 router.delete('/:id', orderItemController.removeOrderItem);
 
 export default router;
--- Routes/orderRoute.js
+++ Routes/orderRoute.js
@@ -1,22 +1,13 @@
 // routes/orderRoutes.js
 import express from 'express';
-import * as orderController from '../controllers/orderController.js';
+import * as orderController from '../Controllers/orderController.js';
 
 const router = express.Router();
 
-// Create a new order
 router.post('/', orderController.createOrder);
-
-// Get orders (optionally filtered by query params)
 router.get('/', orderController.getOrders);
-
-// Get a single order by ID
 router.get('/:id', orderController.getOrderById);
-
-// Update an order (status, payment, delivery, etc.)
 router.put('/:id', orderController.updateOrder);
-
-// Cancel an order (soft delete)
 router.delete('/:id', orderController.cancelOrder);
 
 export default router;
