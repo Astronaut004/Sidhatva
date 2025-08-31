ddb4cae4c5003dc8fcf0a9ed69dc14d6c6de96d9
Astronaut004 <Astronaut004@users.noreply.github.com>
2025-08-31 15:11:30 +0530

    auth middleware

--- updatedserver/server/middleware/authMiddleware.js
+++ updatedserver/server/middleware/authMiddleware.js
@@ -8,7 +8,7 @@ export const protect = asyncHandler(async (req, res, next) => {
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
       token = req.headers.authorization.split(' ')[1];
-      const decoded = jwt.verify(token, config.jwt.secret);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findByPk(decoded.id, {
         attributes: { exclude: ['password_hash'] },
       });
