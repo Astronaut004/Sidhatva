ae7da2939bdd0b64e5714ee2a578a1335fe756b6
Astronaut004 <Astronaut004@users.noreply.github.com>
2025-09-03 00:23:50 +0530

    generate seo title

---
+++ updatedserver/server/utils/seoGenerate.js
@@ -0,0 +1,13 @@
+import generateSlug from "./generateSlug";
+
+export default function generateSeoTitle(name, description) {
+  let base;
+
+  if (description != null) {
+    base = `${name} ${description}`.split(" ").slice(0, 8).join(" "); 
+  } else {
+    base = name.split(" ").slice(0, 8).join(" ");
+  }
+
+  return generateSlug(base);
+}
