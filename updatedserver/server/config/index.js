3f44701045159a0f2ceec249830c25332c6fbd58
Astronaut004 <Astronaut004@users.noreply.github.com>
2025-09-03 18:29:32 +0530

    add refresh var

--- updatedserver/server/config/index.js
+++ updatedserver/server/config/index.js
@@ -13,7 +13,10 @@ const config = {
   jwt: {
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_EXPIRES_IN || '1h',
+    refreshSecret: process.env.JWT_REFRESH_SECRET || process.env.JWT_SECRET,
+    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
   },
+
 };
 
 export default config;
