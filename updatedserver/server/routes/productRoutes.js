28510749c1eca5dddba05cd84f3ccf5a754a290e
Astronaut004 <Astronaut004@users.noreply.github.com>
2025-09-09 20:59:20 +0530

    create route

--- updatedserver/server/routes/productRoutes.js
+++ updatedserver/server/routes/productRoutes.js
@@ -1,8 +1,19 @@
 import express from "express";
 import { createProductHandler } from "../controllers/productController.js";
+import { protect } from "../middleware/authMiddleware.js";
+import { authorize } from "../middleware/roleMiddleware.js";
+import { createProductValidator } from "../validators/productValidators.js";
+import { handleValidationErrors } from "../middleware/validationMiddleware.js";
 
 const router = express.Router();
 
-router.post("/products", createProductHandler);
+router.post(
+  "/",
+  createProductValidator,
+  handleValidationErrors,
+  protect,
+  authorize("admin", "vendor"),
+  createProductHandler
+);
 
 export default router;
